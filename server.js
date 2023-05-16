f0422944b57cf65502ed6e9f5531674f7941cdab
Jorge Mora <jorgemg354@gmail.com>
2023-05-16 15:14:27 -0700

    Change scripts

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const { port, mongoDbUri } = require('./app/config');
 (async () => {
 	try {
 		await dbConnect(mongoDbUri);
-		const app = require('./app');
-		app.listen(port, () => {
-			console.log(`Example app listening on port ${port}`);
-		});
 	} catch (error) {
 		console.log(error);
 	}
 })();
+
+const app = require('./app');
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
