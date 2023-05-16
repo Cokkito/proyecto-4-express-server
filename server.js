8e0a854cc88b7d33c68148d4f04adf37e36a1085
Jorge Mora <jorgemg354@gmail.com>
2023-05-16 15:15:56 -0700

    Attempt 1

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const { dbConnect } = require('./app/db');
 const { port, mongoDbUri } = require('./app/config');
-
-(async () => {
-	try {
-		await dbConnect(mongoDbUri);
-	} catch (error) {
-		console.log(error);
-	}
-})();
-
 const app = require('./app');
+
+dbConnect(mongoDbUri);
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
