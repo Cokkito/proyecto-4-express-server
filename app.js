decdfa5b36d73c76f9f243e6054c3be6d3ccc04e
Jorge Mora <jorgemg354@gmail.com>
2023-05-17 16:21:37 -0700

    add CORS module to express

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 var bodyParser = require('body-parser');
 
+const { corsOrigin } = require('./app/config');
 const usersRouter = require('./app/routes/usersRouter');
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(
+	cors({
+		origin: corsOrigin.split(','),
+		methods: ['GET', 'POST', 'UPDATE', 'DELETE', 'PATCH'],
+		credentials: true,
+	})
+);
 
 app.use('/users', usersRouter);
 
