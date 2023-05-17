b48a9e07db9aab6256012210084ba763c123895c
Jorge Mora <jorgemg354@gmail.com>
2023-05-17 16:50:13 -0700

    add CORS to vercel config

--- app/controller/usersController.js
+++ app/controller/usersController.js
@@ -9,7 +9,6 @@ const createUser = async (req, res) => {
 	try {
 		const { name, email, password } = req.body;
 		const encryptedPassword = cryptr.encrypt(password);
-
 		await User.create({
 			name,
 			email,
@@ -25,7 +24,6 @@ const createUser = async (req, res) => {
 const loginUser = async (req, res) => {
 	try {
 		const { email, password } = req.body;
-
 		const user = await User.findOne({ email }).lean();
 		if (user) {
 			const decryptedPassword = cryptr.decrypt(user.password);
